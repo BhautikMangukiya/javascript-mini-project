97ecdb4ff7184ac33481cae35259c36a2cd4cee8
Bhautik Mangukiya <bhautikmangukiya3386@gmail.com>
2024-04-19 11:54:09 +0530

    first project

---
+++ color switcher/server.js
@@ -0,0 +1,51 @@
+let red = document.querySelector('#red')
+let Green = document.querySelector('#Green')
+let Blue = document.querySelector('#blue')
+let Yellow = document.querySelector('#Yellow')
+let Purple = document.querySelector('#Purple')
+let Cyan = document.querySelector('#Cyan')
+let Orange = document.querySelector('#Orange')
+let Pink = document.querySelector('#Pink')
+let Brown = document.querySelector('#Brown')
+let Teal = document.querySelector('#Teal')
+
+
+red.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "red"
+})
+
+green.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Green"
+})
+
+Blue.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Blue"
+})
+
+Yellow.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Yellow"
+})
+
+Purple.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Purple"
+})
+
+Cyan.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Cyan"
+})
+
+Orange.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Orange"
+})
+
+Pink.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Pink"
+})
+
+Brown.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Brown"
+})
+
+Teal.addEventListener('click' , function(){
+    document.body.style.backgroundColor = "Teal"
+})
